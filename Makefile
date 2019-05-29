8aa1ea14a4243dcb35ea8da378caffb560c3d74e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-05-28 20:40:40 -0500

    add take-random target

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ help:
 	@echo "Use \`make <target>\` where \`<target>\` is one of"
 	@echo "  init             initialize friends"
 	@echo "  retrieve-friends retrieve friends"
+	@echo "  take-random      take n random friends, default n = 10"
 
 init:
 	@$(PYTHON_INTERPRETER) scripts/init_friends_ids.py "$(AUTH_FP)" \
@@ -14,3 +15,6 @@ retrieve-friends:
 	@$(PYTHON_INTERPRETER) scripts/retrieve_friends.py \
 		--auth-filepath _data/auth.json \
 		_data/friends_ids.json
+
+take-random:
+	@$(PYTHON_INTERPRETER) scripts/select_random.py
--- scripts/select_random.py
+++ scripts/select_random.py
@@ -2,17 +2,18 @@ import sys
 import random
 import json
 
-FRIENDS_IDS = 'data/friends_ids.json'
+FRIENDS_IDS = '_data/friends_ids.json'
 PREFIX = 'https://twitter.com/'
 SUFFIX = ''
 N = 10
 
 with open(FRIENDS_IDS) as f:
-    friends = [v for v in json.load(f).values()]
+    friends = [v for v in json.load(f).values() if v]
 
 out = []
 for i in range(N):
     friend = random.choice(friends)
-    out.append(PREFIX + friend['screen_name'] + SUFFIX)
+    friend_screen_name = friend['screen_name']
+    out.append(''.join((PREFIX, friend_screen_name, SUFFIX)))
 
-print(json.dumps(out, indent=4), file=sys.stdout)
+print(json.dumps(out, indent=2), file=sys.stdout)
